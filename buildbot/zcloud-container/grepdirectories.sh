29eba9707c6a734f2c4b967abb1ac3ff2038ec12
Evren Kutar <furiston@gmail.com>
2016-01-04 13:09:11 +0200

    zcloud builder +

--- buildbot/zcloud-container/grepdirectories.sh
+++ buildbot/zcloud-container/grepdirectories.sh
@@ -7,7 +7,7 @@ echo "{
 }" >> ~/.docker/config.json
 
 
-FILES=$(git diff-tree --no-commit-id --name-only -r 1b7484683d45cfa072c6653ce17758327f882a63)
+FILES=$(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD))
 CONTAINERS=(); for f in $FILES; do d=( ${f//\// }); if [[  ${d[0]} == 'containers' ]]; then CONTAINERS+=(${d[1]}); fi; done;
 UNIQUE_CONTAINERS=($(for v in "${CONTAINERS[@]}"; do echo "$v";done| sort| uniq| xargs))
 echo ${UNIQUE_CONTAINERS[@]}
