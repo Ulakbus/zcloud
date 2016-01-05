1136e7c846761667b8fefe70e7bdcbd0e4195562
Evren Kutar <furiston@gmail.com>
2016-01-05 13:52:04 +0200

    do it with image builder py

--- buildbot/zcloud-container/image_builder.py
+++ buildbot/zcloud-container/image_builder.py
@@ -4,8 +4,18 @@
 # (GPLv3).  See LICENSE.txt for details.
 
 import sys
+import os
 import subprocess
 
-
 if __name__ == '__main__':
-	subprocess.check_output("./grepdirectories.sh", shell=True)
+	files = subprocess.check_output("git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD)", shell=True).split('\n')
+	containers = [f.split('/')[1] for f in files if 'containers' in f]
+	unique_containers = set(containers)
+	basepath = '/buildslave/sourcefiles/containers/'
+
+	for i in unique_containers:
+		os.chdir(basepath+i)
+		f = open("Dockerfile", "r")
+		buildname = [line.split('=') for line in f.readlines() if 'BUILDNAME' in line][0][1]
+		subprocess.check_output("docker build -t %s .") % buildname
+		subprocess.check_output("docker push %s") % buildname
