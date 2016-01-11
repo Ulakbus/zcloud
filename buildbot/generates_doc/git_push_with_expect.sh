47aa419172d57c803bcb525d42ab45c8774fd039
Evren Kutar <furiston@gmail.com>
2016-01-11 15:38:02 +0200

    FIX set timeout rref #5019

--- buildbot/generates_doc/git_push_with_expect.sh
+++ buildbot/generates_doc/git_push_with_expect.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/expect
 set user [lindex $argv 0]
 set password [lindex $argv 1]
+set timeout 20
 spawn git push origin gh-pages
 
 expect "Username for 'https://github.com':" { send "$user\r" }
