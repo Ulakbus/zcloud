7f4a14e92c3f98aa16970de957528f8f5b17d830
Evren Kutar <furiston@gmail.com>
2016-01-11 17:56:03 +0200

    fix for push

--- buildbot/generates_doc/git_push_with_expect.sh
+++ buildbot/generates_doc/git_push_with_expect.sh
@@ -3,7 +3,7 @@ set password [lindex $argv 0]
 spawn git push origin gh-pages
 
 expect "Username for 'https://github.com':" { send "zetaopsbot\r" }
-expect "Password for 'https://zetaopsbot@github.com':"
-send "$password\r"
+expect "Password for 'https://zetaopsbot@github.com':" { send "$password\r" }
+expect "Push successfull" { send "\r" }
 
 interact
