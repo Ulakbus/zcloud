35f5c0f7fa1da27322c861fd8de4525694ac657e
Evren Kutar <furiston@gmail.com>
2016-01-11 14:02:30 +0200

    FIX pass env var as argument rref #5019

--- buildbot/generates_doc/git_push_with_expect.sh
+++ buildbot/generates_doc/git_push_with_expect.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/expect
-
+set user [lindex $argv 0]
+set password [lindex $argv 1]
 spawn git push origin gh-pages
 
-expect "Username for 'https://github.com':" { send "$BUILDBOTGITHUBUSER\r" }
-expect "Password for 'https://$BUILDBOTGITHUBUSER@github.com':" { send "$BUILDBOTGITHUBPASS\r" }
+expect "Username for 'https://github.com':" { send "$user\r" }
+expect "Password for 'https://$user@github.com':" { send "$password\r" }
 
 interact
--- buildbot/generates_doc/unite_docs.sh
+++ buildbot/generates_doc/unite_docs.sh
@@ -1,18 +1,18 @@
 cd /buildslave/ulakbus.org
 git checkout -b gh-pages origin/gh-pages
 # cp gh-pages wiki
-cp /buildslave/ulakbus.org/build/html/* /buildslave/ulakbus.org/wiki/
+cp -r /buildslave/ulakbus.org/build/html/* /buildslave/ulakbus.org/wiki/
 echo "gh-pages wiki copied to gh-pages branch"
 
-cp /buildslave/ulakbus/docs/build/html /buildslave/ulakbus.org/ulakbus
+cp -r /buildslave/ulakbus/docs/build/html /buildslave/ulakbus.org/ulakbus
 echo "ulakbus docs copied to gh-pages branch"
 
-cp /buildslave/zengine/docs/build/html /buildslave/ulakbus.org/zengine
+cp -r /buildslave/zengine/docs/build/html /buildslave/ulakbus.org/zengine
 echo "zengine docs copied to gh-pages branch"
 
-cp /buildslave/pyoko/docs/build/html /buildslave/ulakbus.org/pyoko
+cp -r /buildslave/pyoko/docs/build/html /buildslave/ulakbus.org/pyoko
 echo "pyoko docs copied to gh-pages branch"
 
 git add .
 git commit -m "Buildbot generated docs for humanity, thanks..."
-.././git_push_with_expect.sh 
+.././git_push_with_expect.sh $(echo $BUILDBOTGITHUBUSER) $(echo $BUILDBOTGITHUBPASS)
