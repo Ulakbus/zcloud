5da06d965500b9cb5aeb58b0e219b8b874567f51
Evren Kutar <furiston@gmail.com>
2016-01-18 15:11:00 +0200

    EOF fix for expect script

--- buildbot/generates_doc/git_push_with_expect.sh
+++ buildbot/generates_doc/git_push_with_expect.sh
@@ -2,8 +2,8 @@
 set password [lindex $argv 0]
 spawn git push origin gh-pages
 
-expect "Username for 'https://github.com':" { send "zetaopsbot\r" }
-expect "Password for 'https://zetaopsbot@github.com':" { send "$password\r" }
-expect "Push successfull" { send "ok\r" }
+expect "Username for 'https://github.com':" { send "zetaopsbot\r"; exp_continue }
+expect "Password for 'https://zetaopsbot@github.com':" { send "$password\r"; exp_continue }
+expect "Push successfull" { send "ok\r"; exp_continue }
 
 interact
