967cc9cdecc865b26c0bc2853e210d97d5fa3505
Ali Riza Keles <ali.r.keles@gmail.com>
2016-07-22 13:24:28 +0300

    fix unitime mysql env var script bug

--- containers/unitime/entrypoint.sh
+++ containers/unitime/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sed -i 's/localhost:3306/$MYSQL_SERVER_PORT/' /usr/local/tomcat/conf/unitime.properties
+for VAR in MYSQL_SERVER_PORT MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD;
+  do
+    sed -i "s/$VAR/${!VAR}/" /usr/local/tomcat/conf/unitime.properties
+  done
+
 exec /usr/local/tomcat/bin/catalina.sh run
